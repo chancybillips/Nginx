9b49d78ac362a2a4329667553ea65ffcf0ec8355
chance <chancybillips@gmail.com>
2019-02-25 10:13:46 -0600

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,6 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 package 'Nginx'
 
-service 'Nginx' do
-    action :nothing
-end
-
 file '/usr/share/nginx/html/index.html' do
     content '<html>ok</html>'
 end
